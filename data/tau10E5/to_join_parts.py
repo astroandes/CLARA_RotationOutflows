eb50b1966083ae8c56d564c7fe68fd7ee853964e
Maria Camila Remolina Gutierrez <mariacamilaremolinagutierrez@gmail.com>
2015-09-11 23:03:16 -0500

    some comments

--- data/tau10E5/to_join_parts.py
+++ data/tau10E5/to_join_parts.py
@@ -10,13 +10,13 @@ for vrot in vrots:
         path = './vrot'+str(vrot)+'/vout'+str(vout)+'/'
         filename = 'tau10E'+str(logtau)+'_vrot'+str(vrot)+'_vout'+str(vout)
 
-        os.system('rm '+path+filename+'_out.ascii')
+        #os.system('rm '+path+filename+'_out.ascii')
         os.system('cat '+path+filename+'_out* >> temp.txt')
         os.system('echo "x y z x_u y_u z_u x_frec escaped n_scatterings" > '+path+filename+'_out.ascii')
         os.system('grep -v "#" temp.txt >> '+path+filename+'_out.ascii')
         os.system('rm temp.txt')
 
-        os.system('rm '+path+filename+'_in.ascii')
+        #os.system('rm '+path+filename+'_in.ascii')
         os.system('cat '+path+filename+'_in* >> temp.txt')
         os.system('echo "x y z x_u y_u z_u x_frec escaped n_scatterings" > '+path+filename+'_in.ascii')
         os.system('grep -v "#" temp.txt >> '+path+filename+'_in.ascii')
