f1cd8788186d1aade1dd3aa12f478dde401477b6
Maria Camila Remolina Gutierrez <mariacamilaremolinagutierrez@gmail.com>
2015-09-18 07:25:37 -0500

    data for 10E7

---
+++ data/tau10E7/vrot300/vout100/tau10E7_vrot300_vout100.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#PBS -q batch
+#PBS -N tau10E7_vrot300_vout100
+#PBS -l mem=128mb
+#PBS -l nodes=1:ppn=24
+#PBS -M mc.remolina197@uniandes.edu.co
+#PBS -m abe
+
+module load openmpi/1.8.5
+cd /lustre/home/ciencias/fisica/pregrado/mc.remolina197/github/CLARA-MPI/src/
+mpiexec -n 24 ./mine.x /lustre/home/ciencias/fisica/pregrado/mc.remolina197/data/CLARA_RotationOutflowsData/tau10E7/vrot300/vout100/tau10E7_vrot300_vout100.input
