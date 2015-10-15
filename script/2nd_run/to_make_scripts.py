3cb0341a61de2dc8aa7db459249b26b551b7c50f
Maria Camila Remolina Gutierrez <mariacamilaremolinagutierrez@gmail.com>
2015-10-15 17:01:19 -0500

    to run more

--- script/2nd_run/to_make_scripts.py
+++ script/2nd_run/to_make_scripts.py
@@ -1,32 +1,32 @@
 #Example of directory: /lustre/home/ciencias/fisica/pregrado/mc.remolina197/data/CLARA_RotationOutflowsData/tau10E5/vrot0/vout100
 
-ppns = [6, 12]
-logtaus = [5,6]
+ppns = [6, 12, 24]
+logtaus = [5,6,7]
 vrots = [50,100]
 vouts = [25,50,75]
 
-for i in range(2):
+for i in range(3):
 
     logtau = logtaus[i]
     ppn = ppns[i]
-    vrot = vrots[i]
 
-    for vout in vouts:
+    for vrot in vrots:
+        for vout in vouts:
 
-	filename = 'tau10E'+str(logtau)+'_vrot'+str(vrot)+'_vout'+str(vout)
-	path = '/lustre/home/ciencias/fisica/pregrado/mc.remolina197/data/CLARA_RotationOutflowsData/tau10E'+str(logtau)+'/vrot'+str(vrot)+'/vout'+str(vout)+'/'
-	script = open(path+filename+'.sh', 'w')
+            filename = 'tau10E'+str(logtau)+'_vrot'+str(vrot)+'_vout'+str(vout)
+            path = '/lustre/home/ciencias/fisica/pregrado/mc.remolina197/data/CLARA_RotationOutflowsData/tau10E'+str(logtau)+'/vrot'+str(vrot)+'/vout'+str(vout)+'/'
+            script = open(path+filename+'.sh', 'w')
 
-	script.write('#!/bin/sh\n')
-	script.write('#PBS -q batch\n')
-	script.write('#PBS -N '+filename+'\n')
-	script.write('#PBS -l mem=128mb\n')
-	script.write('#PBS -l nodes=1:ppn='+str(ppn)+'\n')
-	script.write('#PBS -M mc.remolina197@uniandes.edu.co\n')
-	script.write('#PBS -m abe\n')
-	script.write('\n')
-	script.write('module load openmpi/1.8.5\n')
-	script.write('cd /lustre/home/ciencias/fisica/pregrado/mc.remolina197/github/CLARA-MPI/src/\n')
-	script.write('mpiexec -n '+str(ppn)+' ./mine.x '+path+filename+'.input')
+            script.write('#!/bin/sh\n')
+            script.write('#PBS -q batch\n')
+            script.write('#PBS -N '+filename+'\n')
+            script.write('#PBS -l mem=128mb\n')
+            script.write('#PBS -l nodes=1:ppn='+str(ppn)+'\n')
+            script.write('#PBS -M mc.remolina197@uniandes.edu.co\n')
+            script.write('#PBS -m abe\n')
+            script.write('\n')
+            script.write('module load openmpi/1.8.5\n')
+            script.write('cd /lustre/home/ciencias/fisica/pregrado/mc.remolina197/github/CLARA-MPI/src/\n')
+            script.write('mpiexec -n '+str(ppn)+' ./mine.x '+path+filename+'.input')
 
-	script.close()
+            script.close()
--- script/2nd_run/to_run_scripts.py
+++ script/2nd_run/to_run_scripts.py
@@ -1,16 +1,13 @@
 import os
 
-logtaus = [5,6]
+logtaus = [5,6,7]
 vrots = [50,100]
 vouts = [25,50,75]
 
-for i in range(2):
-
-    logtau = logtaus[i]
-    vrot = vrots[i]
-
-    for vout in vouts:
-
-	filename = 'tau10E'+str(logtau)+'_vrot'+str(vrot)+'_vout'+str(vout)
-	path = '/lustre/home/ciencias/fisica/pregrado/mc.remolina197/data/CLARA_RotationOutflowsData/tau10E'+str(logtau)+'/vrot'+str(vrot)+'/vout'+str(vout)+'/'
-	os.system('qsub '+path+filename+'.sh')
+for logtau in logtaus:
+    for vrot in vrots:
+        for vout in vouts:
+            filename = 'tau10E'+str(logtau)+'_vrot'+str(vrot)+'_vout'+str(vout)
+            path = '/lustre/home/ciencias/fisica/pregrado/mc.remolina197/data/CLARA_RotationOutflowsData/tau10E'+str(logtau)+'/vrot'+str(vrot)+'/vout'+str(vout)+'/'
+            #os.system('qsub '+path+filename+'.sh')
+            print 'qsub '+path+filename+'.sh'
