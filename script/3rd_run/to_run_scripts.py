317a924c5ac325d006d3db3cc2216f0d805a05cd
Maria Camila Remolina Gutierrez <mariacamilaremolinagutierrez@gmail.com>
2015-10-24 13:09:19 -0500

    scripts for 3rd run

---
+++ script/3rd_run/to_run_scripts.py
@@ -0,0 +1,13 @@
+import os
+
+logtaus = [5,6,7]
+vrots = [50,100]
+vouts = [5,10,15,20]
+
+for logtau in logtaus:
+    for vrot in vrots:
+        for vout in vouts:
+            filename = 'tau10E'+str(logtau)+'_vrot'+str(vrot)+'_vout'+str(vout)
+            path = '/lustre/home/ciencias/fisica/pregrado/mc.remolina197/data/CLARA_RotationOutflowsData/tau10E'+str(logtau)+'/vrot'+str(vrot)+'/vout'+str(vout)+'/'
+            #os.system('qsub '+path+filename+'.sh')
+            print 'qsub '+path+filename+'.sh'
